4bd0225c6e4bfa6f4b683a6eacf9441dfcc569cf
Sergey <2235437@gmail.com>
2023-03-28 00:48:17 +0300

    add all projectFirst

---
+++ src/main/java/org/crucial/ConvertStr.java
@@ -0,0 +1,16 @@
+package org.crucial;
+
+public class ConvertStr {
+    public static String convert(int fizzBuzz) {
+        if (fizzBuzz % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (fizzBuzz % 3 == 0) {
+            return "Fizz";
+        }
+        if (fizzBuzz % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(fizzBuzz);
+    }
+}
---
+++ src/main/java/org/crucial/Main.java
@@ -0,0 +1,11 @@
+package org.crucial;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(ConvertStr.convert(i));
+        }
+    }
+}
