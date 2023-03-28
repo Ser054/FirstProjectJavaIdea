94057087d3a86a928f374eaa83073a3ec37079bf
Sergey <2235437@gmail.com>
2023-03-28 22:27:47 +0300

    reset gradle on v7.6.1

--- src/main/java/org/crucial/Main.java
+++ src/main/java/org/crucial/Main.java
@@ -1,5 +1,7 @@
 package org.crucial;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
